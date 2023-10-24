5fd42fd32f951548547f25e55dbdec7cec0ef840
thaua <thaua19felipe@gmail.com>
2023-10-24 17:38:24 -0300

    feat: filtragem das perguntas por tema(usuario)

--- src/components/CardTema/CardTema.jsx
+++ src/components/CardTema/CardTema.jsx
@@ -1,14 +1,18 @@
 import MediaQuery from "react-responsive"
 import { temas } from "../../data/temas"
 import Icone from "../Icone/Icone"
+import { contextoPagina } from "../../pages/Home/Home"
+import { useContext } from "react"
 
 const CardTema = () => {
+
+    const {definirTema}= useContext(contextoPagina)
 
   return (
     <div className="flex gap-5 flex-wrap justify-center px-1">
         {temas.map((tema) => 
         
-            <div className="w-36 h-36 sm:w-52 sm:h-52 bg-content2 rounded-2xl bg-opacity-60 p-5 text-center flex flex-col justify-between items-center" key={tema.nome}>
+            <div className="w-36 h-36 sm:w-52 sm:h-52 bg-content2 rounded-2xl bg-opacity-60 p-5 text-center flex flex-col justify-between items-center hover:translate-y-[-20px] cursor-pointer transition-transform" key={tema.nome} onClick={() => definirTema(tema.nome)}>
                 <MediaQuery maxWidth={640}>
                     <Icone tema={tema.nome} tamanho={70}/>
                 </MediaQuery>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,64 @@
+import { createContext, useEffect, useState } from "react"
 import CardPergunta from "../../components/CardPergunta/CardPergunta"
 import CardTema from "../../components/CardTema/CardTema"
+import { ArrowFatLineLeft } from "@phosphor-icons/react"
+
+export const contextoPagina = createContext()
 
 const Home = () => {
 
+  const [temaPagina, definirTema]= useState('inicial')
+
+  useEffect(() => {
+    console.log(temaPagina)
+  }, [temaPagina])
+
   return (
-    <div className="lg:w-[1000px] xl:w-[1300px] m-auto sm:px-20 flex flex-col ">
+    <contextoPagina.Provider value={{temaPagina, definirTema}}>
+      <div className="lg:w-[1000px] xl:w-[1300px] m-auto sm:px-20 flex flex-col ">
 
-      <div className="w-full flex flex-col items-center mb-20">
-        <h1 className="text-2xl sm:text-3xl font-bold my-5">
-          <span className="text-content5">Últimas </span> 
-          Perguntas
-        </h1>
-        <CardPergunta filtro="limite" limite={5}/> 
+        {temaPagina == "inicial" ? 
+
+          <>
+            <div className="w-full flex flex-col items-center mb-20">
+              <h1 className="text-2xl sm:text-3xl font-bold my-5">
+                <span className="text-content5">Últimas </span> 
+                Perguntas
+              </h1>
+              <CardPergunta filtro="limite" limite={5}/> 
+            </div>
+
+            <div className="w-full flex flex-col items-center mb-20">
+              <h1 className="text-2xl sm:text-3xl font-bold my-5 text-content5">
+                Temas
+              </h1>
+              <CardTema />
+            </div>
+          </>
+
+          : 
+
+          <>
+            <div className="w-full flex flex-col items-center mb-20 gap-5">
+              <div className="w-full flex flex-col items-center md:flex-row-reverse md:justify-between">
+          
+                <div className="hidden md:block w-40"></div>
+                <h1 className="text-2xl sm:text-3xl font-bold my-5 text-content5">
+                  {temaPagina.toUpperCase()}
+                </h1>
+                <a className="flex items-center gap-2 md:ml-3 cursor-pointer hover:translate-x-[-10px] transition-transform" 
+                onClick={() => definirTema('inicial')}>
+                  <ArrowFatLineLeft size={24} color="#fdfcfc" weight="duotone" />
+                  <p>Voltar aos temas</p>
+                </a>
+              </div>
+              <CardPergunta filtro="tema" tema={temaPagina}/> 
+            </div>
+          </>
+        }
+
       </div>
-
-      <div className="w-full flex flex-col items-center mb-20">
-        <h1 className="text-2xl sm:text-3xl font-bold my-5 text-content5">
-          Temas
-        </h1>
-        <CardTema />
-      </div>
-
-    </div>
+    </contextoPagina.Provider>
   )
 }
 
