79224f4c68724524e491a1443815f56167fd7424
thaua <thaua19felipe@gmail.com>
2023-10-24 14:51:40 -0300

    ci(tailwindconfig): adicionando a familia de fonte Roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto": ["Roboto"] 
+      }
+    },
   },
   darkMode: "class",
   plugins: [nextui({
