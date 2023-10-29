0533f528baeec66b0ea834afd408b23f03eb1bbd
thaua <thaua19felipe@gmail.com>
2023-10-28 23:41:48 -0300

    feat(modal perguntas): adicionado seleção de temas ao modal de gerenciamento de  perguntas

--- src/components/Modais/ModalPerguntas.jsx
+++ src/components/Modais/ModalPerguntas.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react'
+import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Select, SelectItem, useDisclosure } from '@nextui-org/react'
 import P from 'prop-types'
 import { useContext } from 'react'
 import temaContexto from '../../context/TemaContexto'
@@ -7,7 +7,9 @@ import { useGerenciador } from '../../hooks/useGerenciador'
 import EditorTexto from '../EditorTexto/EditorTexto'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { Upload, XCircle } from '@phosphor-icons/react'
+import { Folders, Upload, XCircle } from '@phosphor-icons/react'
+import { temas } from '../../data/temas'
+import Icone from '../Icone/Icone'
 
 
 
@@ -16,19 +18,21 @@ import { Upload, XCircle } from '@phosphor-icons/react'
 // eslint-disable-next-line react/prop-types
 const ModalPerguntas = ({isOpen, acao}) => {
   
-  const {tema} = useContext(temaContexto)
+  const {tema: temaSistema} = useContext(temaContexto)
 
   const { onOpenChange } = useDisclosure()
   const {gerenciamento, gerenciar} = useGerenciadorContexto()
   const gerenciador = useGerenciador()
 
   const [pergunta, definirPergunta] = useState('')
-
   const [resposta, definirResposta] = useState('')
+  const [tema, definirTema] = useState(new Set([]))
 
   useEffect(() => {
     definirPergunta(gerenciamento[0].pergunta)
     definirResposta(gerenciamento[0].resposta)
+    definirTema(new Set([gerenciamento[0].tema]))
+    
   }, [gerenciamento])
 
   const btnCancelar = () => {
@@ -38,33 +42,56 @@ const ModalPerguntas = ({isOpen, acao}) => {
   const btnFinalizar = () => {
     console.log(pergunta)
     console.log(resposta)
+    console.log(tema.currentKey)
     gerenciador.fechar(gerenciar)
   }
 
-
   return (
-    <Modal className={tema} isOpen={isOpen} onOpenChange={onOpenChange} size='5xl' scrollBehavior='inside' >
+    <Modal className={temaSistema} isOpen={isOpen} onOpenChange={onOpenChange} size='5xl' scrollBehavior='inside' >
     <ModalContent>
       {(onClose) => (
         <>
           <ModalHeader className='flex justify-center'>
-            <h1 className="text-2xl sm:text-3xl font-bold my-5 text-foreground">
-              Perguntas
-              <span className="text-content6"> Online</span>
-            </h1>
+            {acao == "edicao" ? 
+              <h1 className="text-2xl sm:text-3xl font-bold my-5 text-foreground">
+                <span className="text-content6">Editar </span>
+                Pergunta
+              </h1>
+              :
+              <h1 className="text-2xl sm:text-3xl font-bold my-5 text-foreground">
+                <span className="text-content6">Adicionar </span>
+                Pergunta
+              </h1>
+            }
           </ModalHeader>
           <ModalBody>
             <div>
-              <Input size={'lg'} className='text-foreground' label="Pergunta:" description="Digite a pergunta" isRequired value={acao == 'edicao' && pergunta} 
+              <Input size={'lg'} className='text-foreground' label="Pergunta:" description="Digite a pergunta" isRequired value={pergunta} 
               onChange={(e) => definirPergunta(e.target.value)}
               />
             </div>
             <div>
               <p className='text-foreground mb-2'>Resposta: *</p>
               <EditorTexto 
-                textoInicial={acao == 'edicao' && resposta}
+                textoInicial={resposta}
                 onChange={definirResposta}
               />
+            </div>
+            <div className='mt-8'>
+              <Select
+                labelPlacement='outside'
+                label="Tema:"
+                items={temas}
+                className="text-foreground"
+                selectedKeys={tema}
+                onSelectionChange={definirTema}
+                startContent={<Folders size={25} color="#fdfcfc" weight="fill" />}
+                description="Selecione o tema dessa pergunta"
+              >
+                {
+                  (tema) => <SelectItem key={tema.nome} startContent={<Icone icone={tema.icone} tamanho={25}/>}>{tema.nome.toUpperCase()}</SelectItem>
+                }
+              </Select>
             </div>
           </ModalBody>
           <ModalFooter>
@@ -91,7 +118,9 @@ const ModalPerguntas = ({isOpen, acao}) => {
     </ModalContent>
     </Modal>
   )
-}
+  }
+
+
 
 ModalPerguntas.propTypes = {
   isOpen: P.bool,
