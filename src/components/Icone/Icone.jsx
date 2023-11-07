cd7986bbde92b6fbf35cab5e02080fc6d304ba9d
thaua <thaua19felipe@gmail.com>
2023-11-07 16:55:56 -0300

    perf(icones): alteracao da sintaxe
    remoção da opcao de enviar um tema para o componente, recebendo agora apenas o nome do icone
    
    BREAKING CHANGE: Os componentes Icones utilizados passando um tema como parametros nao funcionarao
    mais

--- src/components/Icone/Icone.jsx
+++ src/components/Icone/Icone.jsx
@@ -1,26 +1,17 @@
 import { Book, Cards, Code, GraduationCap, OfficeChair } from '@phosphor-icons/react';
 import P from 'prop-types';
-import { temas } from '../../data/temas';
 
 Icone.propTypes = {
-    tema: P.string,
     tamanho: P.number,
     cor: P.string,
     icone: P.string
 }
 
-export default function Icone({tema, tamanho, cor, icone = "nenhum"}) {
+export default function Icone({tamanho, cor, icone}) {
 
-   let iconeRecebido
-
-   if(icone != "nenhum"){
-        iconeRecebido = icone
-   } else {
-        const temaObj =  temas.filter((item) => item.nome == tema)
-        iconeRecebido = temaObj[0].icone
-   }
+  
     
-  switch(iconeRecebido){
+  switch(icone){
         case 'Code':
             return(
                 <Code size={tamanho} color={cor} weight="duotone" />
