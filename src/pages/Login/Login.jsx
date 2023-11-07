ce60bf9ee49b691872f5e59547a0e4b16c50b4bc
thaua <thaua19felipe@gmail.com>
2023-11-07 10:16:15 -0300

    feat(login): adicionado tela de carregamento

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react"
 import { AuthContext } from "../../context/Auth/AuthProvider"
-import { Button, Input } from "@nextui-org/react"
+import { Button, Input, Progress } from "@nextui-org/react"
 import { Eye, EyeSlash } from "@phosphor-icons/react"
 import { useNavigate } from "react-router-dom"
 // import { useEffect } from "react"
@@ -10,6 +10,8 @@ export const Login = () => {
     const [email, setEmail] = useState("")
     const [senha, setSenha] = useState("")
     const [erro, setErro] = useState("")
+
+    const [loading, setLoading] = useState(false)
 
     const navigate = useNavigate()
     const {entrar} = useContext(AuthContext)
@@ -21,12 +23,14 @@ export const Login = () => {
     // },[autorizacao, navigate])
 
     const handleButton = async ()  => {
+        setLoading(true)
         try{
             await entrar(email, senha)
             navigate('/')
         }catch(e){
             setErro(e.message)
         }
+        setLoading(false)
     }
 
     const [isVisible, setIsVisible] = useState(false);
@@ -35,31 +39,46 @@ export const Login = () => {
 
   return (
     <div className="flex flex-col justify-center items-center  gap-5">
-        <h1>login</h1>
-        <hr />
-        <form className="flex flex-col gap-2 w-[300px]">
-            <Input type="text" label="email" isRequired size="sm" onChange={(e) => setEmail(e.target.value)}
-                isInvalid={erro ? true : false}
-                errorMessage={erro}
-            />
-            <Input type={isVisible ? "text" : "password"} label="senha" isRequired size="sm" onChange={(e) => setSenha(e.target.value)} 
-                endContent={
-                    <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
-                    {isVisible ? (
-                        <Eye size={20} color="#fdfcfc" weight="fill" />
-                    ) : (
-                        <EyeSlash size={20} color="#fdfcfc" weight="fill" />
-                    )}
-                    </button>
+        
+        {loading === false ? 
+            <>
+                <h1>login</h1>
+                <hr />
+                <form className="flex flex-col gap-2 w-[300px]">
+                    <Input type="text" label="email" isRequired size="sm" onChange={(e) => setEmail(e.target.value)}
+                    isInvalid={erro ? true : false}
+                    errorMessage={erro}
+                    />
+                    <Input type={isVisible ? "text" : "password"} label="senha" isRequired size="sm" onChange={(e) => setSenha(e.target.value)} 
+                    endContent={
+                        <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                        {isVisible ? (
+                            <Eye size={20} color="#fdfcfc" weight="fill" />
+                        ) : (
+                            <EyeSlash size={20} color="#fdfcfc" weight="fill" />
+                        )}
+                        </button>
+                    }
+                    
+                    isInvalid={erro ? true : false}
+                    errorMessage={erro}
+                />
+                </form>
+                {
+                email == "" || senha == "" ? <Button isDisabled onClick={() => handleButton()}>Entrar</Button> : <Button onClick={() => handleButton()}>Entrar</Button>
                 }
+            </>
+            :
+            <div className="w-full flex flex-col justify-center items-center">
+                <h1 className="text-2xl  mb-5">Verificando suas credenciais</h1>
+                <Progress size="md" isIndeterminate aria-label="Loading..." className="max-w-sm" 
+
+                color="danger"
+                />
                 
-                isInvalid={erro ? true : false}
-                errorMessage={erro}
-            />
-        </form>
-        {
-            email == "" || senha == "" ? <Button isDisabled onClick={() => handleButton()}>Entrar</Button> : <Button onClick={() => handleButton()}>Entrar</Button>
+            </div>
         }
+        
 
     </div>
   )
